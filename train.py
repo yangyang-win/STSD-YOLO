866ca7431484ec9143033af7a6533f6c14184c61
杨阳 <3228574043@qq.com>
2024-12-24 13:02:30 +0800

    commit

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("STSD-YOLOv9.yaml").load('runs/detect/STSD-YOLOv9/weights/best.pt') 
+model = YOLO("STSD-YOLO.yaml").load('runs/detect/STSD-YOLO/weights/best.pt') 
 # Use the model
 if __name__ == '__main__':
     # Use the model
-    results = model.train(data='tt100k.yaml', epochs=300, batch=14, device=1, name="STSD-YOLOv9")
+    results = model.train(data='tt100k.yaml', epochs=300, batch=14, device=1, name="STSD-YOLO")
--- val.py
+++ val.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-model = YOLO("ultralytics/runs/detect/STSD-YOLOv9/weights/best.pt")  # 
+model = YOLO("ultralytics/runs/detect/STSD-YOLO/weights/best.pt")  # 
 
 # Use the model
 if __name__ == '__main__':
